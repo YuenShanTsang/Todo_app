2ffe6245b440807051e0c16184dac2e3f42e8dd5
YuenShanTsang <A00270323@mycambrian.ca>
2023-04-12 01:14:26 -0400

    Added Todo Model and installed EFCore packages

---
+++ Todo_app/Data/TodoContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Todo_app.Data
+{
+	public class TodoContext: DbContext
+	{
+		
+	}
+}
+
---
+++ Todo_app/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo_app.Models
+{
+	public class Todo
+	{
+        public int TodoId { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public bool IsComplete { get; set; }
+    }
+}
+
